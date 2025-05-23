d2f93529f9dfa8ebd3dced516900a710b9bbb50d
Meet <meetacademics123@gmail.com>
2025-05-23 16:33:08 +0530

    data validation:

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from networksecurity.components.data_ingestion import DataIngestion
+from networksecurity.components.data_validation import DataValidation, DataValidationConfig
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
 from networksecurity.entity.config_entity import DataIngestionConfig,TrainingPipelineConfig
@@ -8,12 +9,18 @@ import sys
 if __name__ == '__main__':
     try:
         trainingpipelineconfig = TrainingPipelineConfig()
-        dataingestionconfig=DataIngestionConfig(trainingpipelineconfig)
-        data_ingestion=DataIngestion(dataingestionconfig)
-        logging.info("initiate ingestion configuration")
-        a=data_ingestion.initiate_data_ingestion()
-        print(a)
-    
-    
+        dataingestionconfig = DataIngestionConfig(trainingpipelineconfig)
+        data_ingestion = DataIngestion(dataingestionconfig)
+        logging.info("Initiating data ingestion")
+        
+        data_ingestion_artifact = data_ingestion.initiate_data_ingestion()  # <-- missing this step before
+        
+        data_validation_config = DataValidationConfig(trainingpipelineconfig)
+        data_validation = DataValidation(data_ingestion_artifact, data_validation_config)  # <-- corrected
+        data_validation_artifact = data_validation.initiate_data_validation()
+        
+        logging.info("Data validation completed")
+        print(data_validation_artifact)
+
     except Exception as e:
-        raise NetworkSecurityException(e,sys)
+        raise NetworkSecurityException(e, sys)
--- networksecurity/constant/training_pipeline/__init__.py
+++ networksecurity/constant/training_pipeline/__init__.py
@@ -4,7 +4,6 @@ import pandas as pd
 
 
 # defining column constants for training pipelin
-
 TARGET_COLUMN = 'Result'
 PIPELINE_NAME: str = 'NetworkSecurity'
 ARTIFACT_DIR: str = 'Artifacts'
@@ -13,12 +12,7 @@ FILE_NAME: str = 'phisingData.csv'
 TRAIN_FILE_NAME: str = 'train.csv'
 TEST_FILE_NAME: str = 'test.csv'
 
-
-
-
-
-
-
+SCHEMA_FILE_PATH = os.path.join("data_schema", "schema.yaml")
 
 # data ingestion related constants 
 DATA_INGESTION_COLLECTION_NAME:str = 'NetworkData'
@@ -28,3 +22,13 @@ DATA_INGESTION_FEATURE_STORE_DIR:str = 'feature_store'
 DATA_INGESTION_FEATURE_FILE_NAME:str = 'phisingData.csv'
 DATA_INGESTION_INJESTED_DIR:str = 'ingested'
 DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO:float = 0.2 
+
+# schema file path
+
+
+# data validation related constants
+DATA_VALIDATION_DIR_NAME:str = 'data_validation'
+DATA_VALIDATION_VALID_DIR:str = 'validated'
+DATA_VALIDATION_INVALID_DIR:str = 'invalid'
+DATA_VALIDATION_DRIFT_REPORT_DIR:str = 'drift_report'
+DATA_VALIDATION_DRIFT_REPORT_FILE_NAME:str = 'report.yaml'
